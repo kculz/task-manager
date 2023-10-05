ffc8856bba452b71f1c59480a779abb4195e65b7
kculz <munyamakudzai095@gmail.com>
2023-10-05 21:42:53 +0200

    FEAT: get all user tasks

--- routes/v1/task.php
+++ routes/v1/task.php
@@ -2,8 +2,7 @@
 
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
-use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
-Route::group(['prefix' => 'v1', 'middleware' => ['auth:sanctum', EnsureFrontendRequestsAreStateful::class]], function () {
+Route::group(['prefix'=>'v1', 'middleware'=>'auth:sanctum'],function () {
     Route::apiResource('tasks', TaskController::class);
 });
