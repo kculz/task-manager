27e95383b8e83b6de5e8a125c128780d46fe1039
kculz <munyamakudzai095@gmail.com>
2023-10-05 08:07:39 +0200

    ERROR: on auth 500 server error- with correct credentials

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -18,7 +19,8 @@ class TaskFactory extends Factory
     {
         return [
             'title'=> $this->faker->sentence(),
-            'is_done'=> false
+            'is_done'=> false,
+            'creator_id'=> User::factory()
         ];
     }
 }
